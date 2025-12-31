0c4e1530b5cd71d911a7148864ae5918638881d2
balajimali95 <MALIBALAJI95@users.noreply.github.com>
2025-12-31 21:23:36 +0530

    upate changes

--- server/src/controllers/bookingController.js
+++ server/src/controllers/bookingController.js
@@ -5,9 +5,9 @@ exports.createBooking = async (req, res) => {
     try {
         const booking = new Booking(req.body);
         await booking.save();
-        res.status(201).send(booking);
+        return res.status(201).send(booking);
     } catch (error) {
-        res.status(400).send(error);
+        return res.status(400).send(error);
     }
 };
 
@@ -15,9 +15,9 @@ exports.createBooking = async (req, res) => {
 exports.getAllBookings = async (req, res) => {
     try {
         const bookings = await Booking.find();
-        res.status(200).send(bookings);
+        return res.status(200).send(bookings);
     } catch (error) {
-        res.status(500).send(error);
+        return res.status(500).send(error);
     }
 };
 
@@ -25,25 +25,33 @@ exports.getAllBookings = async (req, res) => {
 exports.getBookingById = async (req, res) => {
     try {
         const booking = await Booking.findById(req.params.id);
+
         if (!booking) {
             return res.status(404).send();
         }
-        res.status(200).send(booking);
+
+        return res.status(200).send(booking);
     } catch (error) {
-        res.status(500).send(error);
+        return res.status(500).send(error);
     }
 };
 
 // Update a booking by ID
 exports.updateBooking = async (req, res) => {
     try {
-        const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const booking = await Booking.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+
         if (!booking) {
             return res.status(404).send();
         }
-        res.status(200).send(booking);
+
+        return res.status(200).send(booking);
     } catch (error) {
-        res.status(400).send(error);
+        return res.status(400).send(error);
     }
 };
 
@@ -51,11 +59,13 @@ exports.updateBooking = async (req, res) => {
 exports.deleteBooking = async (req, res) => {
     try {
         const booking = await Booking.findByIdAndDelete(req.params.id);
+
         if (!booking) {
             return res.status(404).send();
         }
-        res.status(200).send(booking);
+
+        return res.status(200).send(booking);
     } catch (error) {
-        res.status(500).send(error);
+        return res.status(500).send(error);
     }
 };
