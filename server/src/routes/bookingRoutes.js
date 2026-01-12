517090409ab040269b1561b1fe692f71c842a257
balajimali95 <MALIBALAJI95@users.noreply.github.com>
2026-01-12 10:00:54 +0530

    upate changes

--- server/src/routes/bookingRoutes.js
+++ server/src/routes/bookingRoutes.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 
-// Route to create a new booking
+// Create a new booking
 router.post('/', bookingController.createBooking);
 
-// Route to get all bookings
+// Get all bookings
 router.get('/', bookingController.getAllBookings);
 
-// Route to get a specific booking by ID
+// Get a booking by ID
 router.get('/:id', bookingController.getBookingById);
 
-// Route to update a booking by ID
+// Update a booking by ID
 router.put('/:id', bookingController.updateBooking);
 
-// Route to delete a booking by ID
+// Delete a booking by ID
 router.delete('/:id', bookingController.deleteBooking);
 
 module.exports = router;
